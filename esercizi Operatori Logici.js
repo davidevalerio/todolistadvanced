b51c68b182aa8e058d59ec037332b97b53ea4fb2
Dado <davidevalerio1987@gmail.com>
2025-12-05 15:56:42 +0100

    aggiungo esercizi Operatori Logici

---
+++ esercizi Operatori Logici.js
@@ -0,0 +1,41 @@
+// Operatori logici 1
+
+const num1 = 3;
+const num2 = 7;
+
+console.log(num1 > num2 && num1 === num2);
+
+// Es 2
+
+const num1 = 2;
+const num2 = 6;
+
+console.log(num1 < num2 || num1 === num2);
+
+// Es 3
+
+const condizione = !2;
+
+console.log(!condizione);
+
+// Es 4
+
+const valore1 = 5;
+const valore2 = 7;
+
+console.log((valore1 > 0 && valore2 < 20) || valore1 === 5);
+
+// Es 5
+
+const str1 = "marcone";
+const str2 = "treno";
+
+console.log(str1.length > 0 && str2 !== "treno"); 
+
+// Es 6
+
+const valore1 = 7;
+const valore2 = 8;
+const valore3 = 10;
+
+console.log((valore1 > 0 || valore2 < 20) && valore3 >= 10 && valore3 <= 20 );
